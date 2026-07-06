b73ec945eb712aa57f80039f1b74afac4fe6ae6a
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-06 12:59:38 +0100

    KAN-9 Implement task statistics

---
+++ database.py
@@ -0,0 +1,12 @@
+class Database:
+    def __init__(self):
+        pass
+    def get_total_tasks(self):
+        # Implement logic to get total tasks
+        return 10
+    def get_completed_tasks(self):
+        # Implement logic to get completed tasks
+        return 5
+    def get_pending_tasks(self):
+        # Implement logic to get pending tasks
+        return 5
