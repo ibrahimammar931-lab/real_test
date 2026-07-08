1cbc6e4fae60f57c4ee602c681ae13d68a1ca1d4
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-08 21:22:16 +0100

    KAN-9 Implement task statistics

--- routes/tasks.py
+++ routes/tasks.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
-from services.task_service import get_tasks
+from services.task_statistics_service import TaskStatisticsService
+from flask import Blueprint, jsonify
 
-router = APIRouter()
+routes = Blueprint('tasks', __name__)
 
-@router.get("/tasks")
-def list_tasks():
-    return get_tasks()
+task_statistics_service = TaskStatisticsService()
+
+@routes.route('/tasks/statistics', methods=['GET'])
+def get_task_statistics():
+    total_tasks = task_statistics_service.get_total_tasks()
+    completed_tasks = task_statistics_service.get_completed_tasks()
+    pending_tasks = task_statistics_service.get_pending_tasks()
+    return jsonify({'total_tasks': total_tasks, 'completed_tasks': completed_tasks, 'pending_tasks': pending_tasks})
