b83d87ecce6b81859ba03c1556e6a3f898a826dd
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-09 09:19:40 +0100

    KAN-6 load the students in the main code from another file named students.json that have the content below....you should create the students.json

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from routes.tasks import router as task_router
+import json
 
 app = FastAPI()
 
 app.include_router(task_router)
+
+try:
+    with open('students.json') as f:
+        students_data = json.load(f)
+except FileNotFoundError:
+    print("The students.json file was not found.")
+    students_data = []
+except json.JSONDecodeError:
+    print("Error parsing the students.json file.")
+    students_data = []
+
+def get_students():
+    return students_data
