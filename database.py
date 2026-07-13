e2ebf9e5b98a11a9896898a534524af865301632
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-13 12:49:26 +0100

    KAN-6 load the students in the main code from another file named students.json that have the content below....you should create the students.json

--- database.py
+++ database.py
@@ -1,13 +1,6 @@
 import json
 
 def load_students():
-    try:
-        with open('students.json') as f:
-            data = json.load(f)
-        return data['students']
-    except FileNotFoundError:
-        print('The students.json file was not found.')
-        return []
-    except json.JSONDecodeError:
-        print('The students.json file is not valid JSON.')
-        return []
+    with open('students.json') as f:
+        data = json.load(f)
+    return data['students']
