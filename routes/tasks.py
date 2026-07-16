f766092eea4717da495859d96a0488512bb8d6af
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-16 11:38:25 +0100

    KAN-8 Add email notification support when a new task is created.

--- routes/tasks.py
+++ routes/tasks.py
@@ -1,17 +1,8 @@
 from services.task_service import TaskService
 from services.email_service import EmailService
 
-class Task:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
+email_service = EmailService('smtp_server', 587, 'from_email', 'password')
+task_service = TaskService(email_service)
 
-class TasksRoute:
-    def __init__(self, task_service):
-        self.task_service = task_service
-    
-    def create_task(self, task):
-        return self.task_service.create_task(task)
-
-task_service = TaskService(EmailService('smtp.example.com', 465, 'sender@example.com', 'password'))
-tasks_route = TasksRoute(task_service)
+def create_task(task_name, to_email):
+    task_service.create_task(task_name, to_email)
