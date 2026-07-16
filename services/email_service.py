244c1d6708dd6220b19de72a204f5929b3eae981
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-16 11:38:20 +0100

    KAN-8 Add email notification support when a new task is created.

--- services/email_service.py
+++ services/email_service.py
@@ -1,6 +1,7 @@
-from email.message import EmailMessage
-from email.utils import formatdate
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import logging
 
 class EmailService:
     def __init__(self, smtp_server, smtp_port, from_email, password):
@@ -8,15 +9,20 @@ class EmailService:
         self.smtp_port = smtp_port
         self.from_email = from_email
         self.password = password
-    
-    def send_task_created_email(self, task):
-        msg = EmailMessage()
-        msg['Subject'] = 'New Task Created'
-        msg['From'] = self.from_email
-        msg['To'] = 'recipient@example.com'
-        msg['Date'] = formatdate(localtime = True)
-        msg.set_content(f'Task {task.id} created: {task.name}')
-        
-        with smtplib.SMTP_SSL(self.smtp_server, self.smtp_port) as smtp:
-            smtp.login(self.from_email, self.password)
-            smtp.send_message(msg)
+
+    def send_task_created_email(self, to_email, task_name):
+        try:
+            msg = MIMEMultipart()
+            msg['From'] = self.from_email
+            msg['To'] = to_email
+            msg['Subject'] = 'New Task Created'
+            body = f'A new task {task_name} has been created.'
+            msg.attach(MIMEText(body, 'plain'))
+            server = smtplib.SMTP(self.smtp_server, self.smtp_port)
+            server.starttls()
+            server.login(self.from_email, self.password)
+            text = msg.as_string()
+            server.sendmail(self.from_email, to_email, text)
+            server.quit()
+        except Exception as e:
+            logging.error(f'Error sending email: {e}')
