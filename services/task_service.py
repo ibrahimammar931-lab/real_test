6bc913d3d85f92391818d57f07aac3934240bdf5
Ibrahim Ammar <ibrahimammar931@gmail.com>
2026-07-16 11:37:32 +0100

    KAN-8 Add email notification support when a new task is created.

--- services/task_service.py
+++ services/task_service.py
@@ -1,4 +1,10 @@
-def get_tasks():
-    return [
-        {"id": 1, "title": "Buy milk"}
-    ]
+from services.email_service import EmailService
+
+class TaskService:
+    def __init__(self, email_service):
+        self.email_service = email_service
+    
+    def create_task(self, task):
+        # Task creation logic
+        self.email_service.send_task_created_email(task)
+        return task
